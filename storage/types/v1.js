0c8824be47bbf8a820d0d2d50b1d6a8b663d7f86
Victor França Lopes <victorfranlopes@outlook.com>
2019-08-21 18:12:21 -0300

    🆕 - add code or file(s)(domain): none
    - Add storage domain

---
+++ storage/types/v1.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var FileProcessor;
+(function (FileProcessor) {
+    FileProcessor[FileProcessor["NONE"] = 0] = "NONE";
+})(FileProcessor = exports.FileProcessor || (exports.FileProcessor = {}));
+var Status;
+(function (Status) {
+    Status[Status["STORED"] = 0] = "STORED";
+    Status[Status["UPLOADING"] = 1] = "UPLOADING";
+    Status[Status["FAILED"] = 2] = "FAILED";
+})(Status = exports.Status || (exports.Status = {}));
